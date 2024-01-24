a82e244d84883d0a18bfc3cacc63ec2d1880722c
admin <admin@DESKTOP-RGF6FSL>
2024-01-25 02:05:24 +0530

    Add project files.

---
+++ Linqexample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+class Linqexpmpl
+{
+    public static void Main(string[] args)
+    {
+        List<int> lst=new List<int>() { 1,2,3,4,5,6,7,8,9,10};
+        //linq query to select odd number
+        var oddnumber=from num in lst
+                      where num%2 != 0 && num>1
+                      select num;
+        Console.WriteLine("oddnumber are:");
+        foreach(var num in oddnumber)
+        {
+            Console.WriteLine(num+" ");//o/p:3,5,7,9
+        }
+
+    }
+}
